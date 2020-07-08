ce2a090002faba91b18f732d74ca02502c5e5c76
Suvarna Gaikwad <40233693+suvarawg10@users.noreply.github.com>
2020-07-09 03:25:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+	/*function chess()
+	{
+		document.getElementById("hidden").style.display="block";
+		var color=document.getElement("option").innerHTML;
+		{{color}}
+	}
+	*/
+	document.write("hi this is javasript");
+	window.onload=function();
+	
+	{
+		var body=document.getElementByTagName("body")[0];
+		var table=document.createElement('table');
+		for(var i=0;i<8;i++)
+		{
+			var td=document.createElement('TD');
+			//for(var i=0;i<8;i++)
+			{tr.appendChild(td);
+			}
+		}
+		body.appendChild(table);
+
+	}
